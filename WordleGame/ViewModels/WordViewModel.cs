72b912bb651635ffeb58a11d732254af23373cb2
Sean Murray <G00426904@ATU.IE>
2024-12-16 15:05:15 +0000

    Added check for an existing word list file. Create new file if it does not exist.

--- WordleGame/ViewModels/WordViewModel.cs
+++ WordleGame/ViewModels/WordViewModel.cs
@@ -10,27 +10,67 @@ public class WordViewModel : ContentPage
 	public List<string> WordList { get; set; }
 	private readonly HttpClient httpClient;
 
+	// Define filename for where words will be stored in local storage
+	private const string WordsFileStorageName = "wordlewords.txt";
+
 	public WordViewModel()
 	{
 		httpClient = new HttpClient();
 		WordList = new List<string>();
 	}
 
-    // Loads the words from the link
+    // Loads the wordle words
+	// I used the website below to help me with the string splitting
     // https://stackoverflow.com/questions/46624144/read-words-from-file-into-list
     public async Task LoadWords()
 	{
+		// DirectorySeparatorChar is used instead of "\" as this makes it cross platform as Linux/Mac "/" is used to separate paths
+		string localFilePath = FileSystem.AppDataDirectory + Path.DirectorySeparatorChar + WordsFileStorageName;
 
-		var response = await httpClient.GetStringAsync("https://raw.githubusercontent.com/DonH-ITS/jsonfiles/main/words.txt");
+		if (File.Exists(localFilePath))
+		{
+			var fileContent = File.ReadAllText(localFilePath);
 
-        if (string.IsNullOrWhiteSpace(response))
-        {
-			// Custom exception
-            throw new EmptyWordListException(); 
+			if (string.IsNullOrWhiteSpace(fileContent))
+			{
+				throw new EmptyWordListException();
+			}
+			WordList = fileContent.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Debug.WriteLine("\n\n\nRead words from file\n\n\n");
+
         }
+        else
+		{
+			await DownloadWords(localFilePath);
+		}
+	}
 
-        WordList = response.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+	// Read from webpage and write to file if localFilePath is empty
+	// My brother helped me with some of this
+	public async Task DownloadWords(string localFilePath)
+	{
+		try
+		{
+			var webpageContent = await httpClient.GetStringAsync("https://raw.githubusercontent.com/DonH-ITS/jsonfiles/main/words.txt");
 
+			if (string.IsNullOrWhiteSpace(webpageContent))
+			{
+				// Custom exception
+				throw new EmptyWordListException("Downloaded word list is empty or not loaded correctly.");
+			}
+
+			File.WriteAllText(localFilePath, webpageContent);
+
+			WordList = webpageContent.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Debug.WriteLine("\n\n\nDownloaded words\n\n\n");
+        }
+		catch (Exception ex)
+		{
+			Debug.WriteLine($"Error downloading word list: {ex.Message}");
+			throw new EmptyWordListException("Failed to download or save word list.");
+		}
 	}
 
 
